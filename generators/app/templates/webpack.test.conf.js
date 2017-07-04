eebf757f95142db9ffabe7dae0b84df98437da4f
zhangdi <mkdeeee@gmail.com>
2017-07-04 11:38:08 +0800

    webpack config

---
+++ generators/app/templates/webpack.test.conf.js
@@ -0,0 +1,71 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var mainDirectory = './main.js';
+var presets = ['react', 'es2015', 'stage-0'];
+var entry = [mainDirectory];
+
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    devtool: false,
+    entry: entry,
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components|test)/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: presets,
+                        plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy', 'antd']
+                    }
+                },
+
+            },
+            {
+                test: /\.css?$/,
+                use: ['style-loader','raw-loader','autoprefixer-loader']
+            },
+            {
+                test: /\.less?$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'less-loader'
+                ]
+            },
+            {
+                test: /\.scss?$/,
+                use: ['style-loader','css-loader','sass-loader','autoprefixer-loader']
+            },
+            {
+                test: /\.(png|jpg|)$/,
+                use: ['url-loader?limit=10000']
+                //loader: 'url-loader?limit=250'
+            }
+        ]
+    },
+    output: {
+        path: path.join(__dirname, '/build/dist'),
+        filename: "[name].min.js",
+        chunkFilename: '[id]_[hash].chunk.min.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: '../index.html',
+            template: path.join(__dirname, 'index-template-test.html'),
+            favicon: path.join(__dirname, '/src/public/favicon.ico')
+        })
+    ]
+};
